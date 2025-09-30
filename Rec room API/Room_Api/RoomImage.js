6ae4503d8b24fe8cc491ffc40c8f766cd3546a81
Insxnia <153448876+Insxnia@users.noreply.github.com>
2025-09-30 22:14:41 +0200

    Player & Room Apis

---
+++ Rec room API/Room_Api/RoomImage.js
@@ -0,0 +1,36 @@
+if (!window.location.href.includes("rec.net")) {
+    alert("Not in RecNet! https://rec.net")
+    throw new Error("Not in RecNet!")
+}
+ 
+
+auth_storage = JSON.parse(localStorage.getItem("na_current_user_session"))
+if (!auth_storage) {
+    alert("Not logged in!")
+    throw new Error("Haven't logged in!")
+}
+
+AToken = auth_storage.accessToken
+
+acc_token = "Bearer " + (AToken)
+
+
+img = prompt("Please enter the img.rec.net link!")
+
+img_name = img.split("/").at(-1).split("?").at(0)
+
+r = await fetch("https://rooms.rec.net/rooms/931444637734012214/image", {
+    "headers": {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Authorization": acc_token
+    },
+    "body": "imageName=" + img_name,
+    "method": "PUT"})
+
+    is_json = r.headers.get('content-type').includes("application/json")
+if (!is_json) {
+    alert("You didn't input a valid link!")
+    throw new Error("Didn't input a valid link!")}
+
+    r_json = await r.json()
+alert("Success: " + r_json.success + "\nError: " + r_json.error)
